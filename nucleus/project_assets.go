b5655e536f2120c772a5b52c936fe430084c57bb
rsclarke <hey@rsclarke.dev>
2021-03-26 12:01:01 +0800

    Updated types for GetAsset

--- nucleus/project_assets.go
+++ nucleus/project_assets.go
@@ -6,19 +6,20 @@ import (
 	"net/http"
 )
 
-type DataSensitivity int
-type Compliance int
+type DataSensitivity string
+type Compliance string
 
 const (
-	Low      DataSensitivity = 2
-	Moderate DataSensitivity = 5
-	High     DataSensitivity = 7
-	Critical DataSensitivity = 10
+	Low      DataSensitivity = "2"
+	Moderate DataSensitivity = "5"
+	High     DataSensitivity = "7"
+	Critical DataSensitivity = "10"
 
-	No  Compliance = 5
-	Yes Compliance = 10
+	No  Compliance = "5"
+	Yes Compliance = "10"
 )
 
+// Asset contains the property which describes an asset.
 type Asset struct {
 	OperatingSystemVersion string                 `json:"operating_system_version"`
 	OperatingSystemName    string                 `json:"operating_system_name"`
@@ -28,21 +29,24 @@ type Asset struct {
 	Users                  []string               `json:"asset_users"`
 	Location               string                 `json:"asset_location"`
 	Criticality            string                 `json:"asset_criticality"`
+	CriticalityScore       string                 `json:"asset_criticality_score"`
 	Active                 bool                   `json:"active"`
 	ImageDistro            string                 `json:"image_distro"`
 	IPAddress              string                 `json:"ip_address"`
 	Notes                  string                 `json:"asset_notes"`
 	ID                     string                 `json:"asset_id"`
 	Name                   string                 `json:"asset_name"`
-	Groups                 []string               `json:"asset_groups"`
+	MatchName              string                 `json:"asset_match_name"`      // not in swagger
+	MatchNameLink          string                 `json:"asset_match_name_link"` // not in swagger
+	Groups                 util.EmptyStrAsSlice   `json:"asset_groups"`          // GetAsset returns the empty string "" for groups instead of the empty array
 	ImageRepo              string                 `json:"image_repo"`
 	Info                   map[string]interface{} `json:"asset_info"`
 	URL                    string                 `json:"url"`
 	DomainName             string                 `json:"domain_name"`
-	ComplianceScore        Compliance             `json:"asset_compliance_score"`
+	ComplianceScore        Compliance             `json:"asset_complianced_score"`
 	Type                   string                 `json:"asset_type"`
 	MACAddress             string                 `json:"mac_address"`
-	Decommissioned         bool                   `json:"decommed"`
+	Decommissioned         string                 `json:"decommed"`
 	ParentHostID           string                 `json:"parent_host_id"`
 	ImageTag               string                 `json:"image_tag"`
 }
